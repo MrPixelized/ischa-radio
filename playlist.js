46d4284ccb1e8f0d1af53f3c08c6e7c4c1c6c59f
Haouari haitam Kouider <57036855+haouarihk@users.noreply.github.com>
2022-03-04 17:02:24 +0100

    Some little optimizations and better formatting
    :>

--- playlist.js
+++ playlist.js
@@ -1,134 +1,149 @@
+function getTrackIndex(str) {
+  return str.split(":")[0];
+}
+
+function HighlightElement(element) {
+  element.style.color = "var(--visited)";
+  element.id = "playlist-current";
+}
+
+function getTrackTitle(str) {
+  return str.split(" ").slice(1).join(" ");
+}
+
+function viewElement(element) {
+  element.scrollIntoView({
+    block: "end",
+    inline: "end",
+    behavior: "smooth",
+  });
+}
+
+function formatTrack(track) {
+  return `${track.index}. ${track.full_title}`;
+}
+
 class Track {
-	constructor(str) {
-		this.index = str.split(':')[0];
+  constructor(str) {
+    this.index = getTrackIndex(str);
+    this.full_title = getTrackTitle(str);
+  }
 
-		let [_, ...title] = str.split(' ');
-		this.full_title =  title.join(' ');
-	}
-
-	get format() {
-		return this.index + '. ' + this.full_title;
-	}
+  get format() {
+    return formatTrack(this);
+  }
 }
 
 class UpdateStreamer {
-	constructor() {
-		this.sse = new EventSource('https://radio.ischa.dev/status');
-		this.event_handlers = [];
-		this.sse.onmessage = async (e) => {
-			for (let handler of this.event_handlers) {
-				await handler(e);
-			}
-		}
-	}
+  constructor() {
+    this.sse = new EventSource("https://radio.ischa.dev/status");
+    this.event_handlers = [];
+    this.sse.onmessage = async (e) => {
+      for (let handler of this.event_handlers) {
+        await handler(e);
+      }
+    };
+  }
 
-	add_handler(event_handler) {
-		this.event_handlers.push(event_handler);
-	}
+  add_handler(event_handler) {
+    this.event_handlers.push(event_handler);
+  }
 }
 
 class MpdData {
-	constructor() {
-		this.streamer = new UpdateStreamer();
-		this.tracks = null;
-		this.current = null;
+  constructor() {
+    this.streamer = new UpdateStreamer();
+    this.tracks = null;
+    this.current = null;
 
-		this.streamer.add_handler(this.update.bind(this));
-	}
+    this.streamer.add_handler(this.update.bind(this));
+  }
 
-	async update(e = null) {
-		if (e === null) {
-			await this.update_tracks();
-		} else {
-			if (e.data == 'playlist') {
-				await this.update_tracks();
-			}
-		}
+  async update(e = null) {
+    if (e === null || e.data === "playlist") await this.update_tracks();
 
-		await this.update_current();
-	}
+    await this.update_current();
+  }
 
-	async update_tracks() {
-		let tracks = await fetch('https://radio.ischa.dev/status/playlist');
-		tracks = await tracks.text();
-		tracks = tracks.trim().split('\n');
-		
-		this.tracks = [];
+  async update_tracks() {
+    const tracks = await fetch("https://radio.ischa.dev/status/playlist").then(
+      (c) => c.text()
+    );
 
-		for (let str of tracks) {
-			this.tracks.push(new Track(str));
-		}
-	}
+    this.tracks = tracks
+      .trim()
+      .split("\n")
+      .map((str) => new Track(str));
+  }
 
-	async update_current() {
-		let current = await fetch('https://radio.ischa.dev/status/current');
-		current = await current.text();
-		current = new Track(current);
+  async update_current() {
+    const current = await fetch("https://radio.ischa.dev/status/current").then(
+      (c) => c.text()
+    );
 
-		this.current = this.tracks[current.index - 1];
-	}
+    this.current = this.tracks[getTrackIndex(current) - 1];
+  }
 }
 
 const mpd_data = new MpdData();
 
 class Playlist extends HTMLElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.shadow = this; //.attachShadow({mode: 'open'});
+    this.shadow = this; //.attachShadow({mode: 'open'});
 
-		this.data = mpd_data;
-		this.data.streamer.add_handler(this.render.bind(this))
+    this.data = mpd_data;
+    this.data.streamer.add_handler(this.render.bind(this));
 
-		this.render();
-	}
+    this.render();
+  }
 
-	async render(_e) {
-		if (!this.data.current) {
-			await this.data.update();
-		}
+  async render(_e) {
+    if (!this.data.current) await this.data.update();
 
-		this.shadow.innerHTML = '';
+    // clean up the shadow dom
+    this.shadow.innerHTML = "";
 
-		for (let track of this.data.tracks) {
-			let element = document.createElement('p');
-			element.innerHTML = track.index + '. ' + track.full_title;
+    // this will be used to identify the current track's element
+    let currentElement;
 
-			if (this.data.current== track) {
-				element.style.color = 'var(--visited)';
-				element.id = "playlist-current";
-				element.scrollIntoView({
-					block: 'end',
-					inline: 'end',
-					behavior: 'smooth',
-				});
-			}
+    // add the tracks to the shadow dom
+    this.shadow.append(
+      ...this.data.tracks.map((track) => {
+        const element = document.createElement("p");
+        element.innerHTML = formatTrack(track);
 
-			this.shadow.appendChild(element);
-		}
-	}
+        // if the track is the current track, highlight it
+        if (track == this.data.current)
+          HighlightElement((currentElement = element));
+
+        return element;
+      })
+    );
+
+    if (currentElement) viewElement(currentElement);
+  }
 }
 
 class CurrentTrack extends HTMLElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		this.shadow = this; //.attachShadow({mode: 'open'});
+    this.shadow = this; //.attachShadow({mode: 'open'});
 
-		this.data = mpd_data;
-		this.data.streamer.add_handler(this.render.bind(this))
+    this.data = mpd_data;
+    this.data.streamer.add_handler(this.render.bind(this));
 
-		this.render();
-	}
+    this.render();
+  }
 
-	async render(_e) {
-		if (!this.data.current) {
-			await this.data.update();
-		}
+  async render(_e) {
+    if (!this.data.current) await this.data.update();
 
-		this.shadow.innerHTML = this.data.current.format;
-	}
+    this.shadow.innerHTML = this.data.current.format;
+  }
 }
 
-customElements.define('live-playlist', Playlist);
-customElements.define('current-track', CurrentTrack);
+customElements.define("live-playlist", Playlist);
+customElements.define("current-track", CurrentTrack);
