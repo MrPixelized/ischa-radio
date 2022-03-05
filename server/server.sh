9c736f79a2a0111d0dfc0c6b54afe7aacd588eed
Ischa Abraham <contact@ischa.dev>
2022-03-05 18:11:56 +0100

    added backend server

---
+++ server/server.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# delimiter for blocks to be sent over HTTP
+delim="___"
+
+# format of shown tracks
+mpcfmt="%position%:%id%. %artist% - %title%"
+
+# make sure child processes are killed when this one is
+trap 'trap - TERM; kill 0' INT TERM QUIT EXIT
+
+# outputs an SSE stream containing changed elements of the music
+# server (playlist, player, etc.), delimited by $delim
+function mpc-statusloop() {
+	mpc
+
+	mpc idleloop | while read change; do
+		printf "event: update_to\n\n"
+		printf "data: $change\n\n"
+		printf "\n\n"
+		echo "$delim"
+	done
+}
+
+# whenever the current track changes, outputs the current track,
+# new tracks are delimited by $delim
+function current-update() {
+	mpc current -f "$mpcfmt"
+	echo "$delim"
+
+	mpc idleloop | while read change; do
+		if [ $change == "player" ]; then
+			mpc current -f "$mpcfmt"
+			echo "$delim"
+		fi
+	done
+}
+
+# whenever the playist changes, outputs the playlist,
+# new playlists are delimited by $delim
+function playlist-update() {
+	mpc playlist -f "$mpcfmt"
+	echo "$delim"
+
+	mpc idleloop | while read change; do
+		if [ $change == "playlist" ]; then
+			mpc playlist -f "$mpcfmt"
+			echo "$delim"
+		fi
+	done
+}
+
+# TODO: use UNIX domain sockets instead, in the location the web API
+# would expect to find them
+
+# pipe the current playlist, current track and SSE status updates
+# to HTTP endpoints
+mpc-statusloop  | ssimoe 127.0.0.1:6601 -d "$delim" &
+current-update  | ssimoe 127.0.0.1:6602 -d "$delim" -m 1 -b 1 &
+playlist-update | ssimoe 127.0.0.1:6603 -d "$delim" -m 1 -b 1 &
+
+wait
